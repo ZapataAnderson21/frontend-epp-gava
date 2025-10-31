ba0869d9a03d0536e7348bf7227daa45cb4370b4
ZapataAnderson21 <u21208915@utp.edu.pe>
2025-10-31 15:56:11 -0500

    feat: enhance PettyCash component with form handling, save modal, and expense type selection

--- src/modules/Projects/PettyCashes/PettyCash.tsx
+++ src/modules/Projects/PettyCashes/PettyCash.tsx
@@ -1,9 +1,12 @@
-import { ReturnButton } from "../../../common/button";
-import { Form, InputForm } from "../../../common/form";
+import { useEffect, useState } from "react";
+import { ReturnButton, SaveButton } from "../../../common/button";
+import { ButtonContainer, Form, InputForm, SaveModal, SelectForm } from "../../../common/form";
 import { pettyCashApi } from "../../../data/apiUrl";
 import type { PettyCashType } from "../../../data/types";
-import { useFetch } from "../../../hooks";
+import { useApiAction, useFetch } from "../../../hooks";
 import { toDateLocalValue } from "../../../utils";
+import { LoadingSkeletonForm } from "../../../common/loading";
+import { ErrorMessage } from "../../../common/error";
 
 interface PettyCashProps {
   pettyCashId: number;
@@ -12,45 +15,106 @@ interface PettyCashProps {
 
 export default function PettyCash({ pettyCashId, closeAction }: PettyCashProps) {
 
-  const {data: pettyCash, error, loading} = useFetch<PettyCashType>(`${pettyCashApi}${pettyCashId}`);
+  const {data: pettyCash, error: fetchError, loading} = useFetch<PettyCashType>(`${pettyCashApi}${pettyCashId}`);
+  const [expenseType, setExpenseType] =  useState<string>("");
+  const [amount, setAmount] =  useState<number>(0);
+  const [invoiceNumber, setInvoiceNumber] =  useState<string>("");
+  const [expenseDate, setExpenseDate] =  useState<string>("");
+  const [description, setDescription] =  useState<string>("");
+
+  const {execute, loading: saving} = useApiAction<PettyCashType>();
+
+  const [openSaveModal, setOpenSaveModal] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
+  const [error, setError] = useState(false);
+
+  const [onOk, setOnOk] = useState<() => void>(() => () => {});
+
+  useEffect(() => {
+    if (pettyCash) {
+      setExpenseType(pettyCash.expenseType);
+      setAmount(pettyCash.amount);
+      setInvoiceNumber(pettyCash.invoiceNumber);
+      setExpenseDate(pettyCash.expenseDate);
+      setDescription(pettyCash.description);
+    }
+  }, [pettyCash]);
+
+  const handleUpdate = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setOpenSaveModal(true);
+
+    const updatedPettyCash = {
+      expenseType,
+      amount,
+      invoiceNumber,
+      expenseDate,
+      description,
+    };
+
+    const res = await execute(`${pettyCashApi}${pettyCashId}`, "PATCH", updatedPettyCash);
+    
+    setSuccessMessage(res.message);
+    setOpenSaveModal(false);
+
+    if(res.statusCode == 200) {
+      setOnOk(() => () => {
+        setOpenSaveModal(false);
+        closeAction();
+      });
+    } else {
+      setError(true);
+    }
+  }
+
+  if(loading) return <LoadingSkeletonForm numberRows={6} />;
+  if(fetchError) return <ErrorMessage errorMessage="Error al cargar la salida de caja chica" />;
 
   return (
    <div className="bg-white rounded-xl w-xl overflow-auto max-h-full">
-      <Form name={pettyCash ? `Detalle de salida de caja chica: ${pettyCash.pettyCashId}` : loading ? "Cargando..." : error ? "Error" : "Salida de caja chica no encontrada"} handleSubmit={() => {}}>
+      <Form name={pettyCash ? `Detalle de salida de caja chica: ${pettyCash.pettyCashId}` : loading ? "Cargando..." : error ? "Error" : "Salida de caja chica no encontrada"} handleSubmit={handleUpdate} >
         
-        <InputForm
+        <SelectForm
           label="Tipo de Gasto"
           name="expenseType"
-          value={pettyCash ? pettyCash.expenseType : ""}
-          type="text"
-          onChange={() => {}}
-          disabled
+          value={expenseType}
+          onChange={(value) => setExpenseType(value)}
+          options={[
+            { value: "meals", label: "Alimentación" },
+            { value: "fuel", label: "Combustible" },
+            { value: "transport", label: "Transporte" },
+            { value: "supplies", label: "Materiales / Insumos" },
+            { value: "safety_equipment", label: "Equipo de Seguridad" },
+            { value: "services", label: "Servicios" },
+            { value: "other", label: "Otros" }
+          ]}
         />
         
         <InputForm
           label="Monto"
           name="amount"
-          value={pettyCash ? pettyCash.amount : ""}
+          value={amount}
           type="number"
-          onChange={() => {}}
+          onChange={(e) => setAmount(Number(e.target.value))}
           disabled
         />
 
         <InputForm
           label="Número de Comprobante"
           name="invoiceNumber"
-          value={pettyCash ? pettyCash.invoiceNumber : ""}
+          value={invoiceNumber}
           type="text"
-          onChange={() => {}}
+          onChange={(e) => setInvoiceNumber(e.target.value)}
           disabled
         />
 
         <InputForm
           label="Fecha del Gasto"
           name="expenseDate"
-          value={pettyCash ? toDateLocalValue(pettyCash.expenseDate) : ""}
+          value={expenseDate}
           type="date"
-          onChange={() => {}}
+          onChange={(e) => setExpenseDate(e.target.value)}
           disabled
         />
 
@@ -66,14 +130,25 @@ export default function PettyCash({ pettyCashId, closeAction }: PettyCashProps) 
         <InputForm
           label="Descripción"
           name="description"
-          value={pettyCash ? pettyCash.description : ""}
+          value={description}
           type="text"
-          onChange={() => {}}
+          onChange={(e) => setDescription(e.target.value)}
           disabled
         />
 
-        <ReturnButton onClick={closeAction} />
+        <ButtonContainer>
+          <ReturnButton onClick={closeAction} />
+          <SaveButton loading={saving} />
+        </ButtonContainer>
+
       </Form>
+      {openSaveModal && (
+        <SaveModal
+          onOk={onOk}
+          message={successMessage}
+          error={error}
+        />
+      )}
     </div>
   );
 }
