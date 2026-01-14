0b4d09e5436ee38060cf1130e3a5bd5ddd17c785
ZapataAnderson21 <u21208915@utp.edu.pe>
2026-01-14 10:45:38 -0500

    fix: Wrap NavbarItemProject components with Permission for access control

--- src/modules/Projects/routes/Project/outlet/PurchaseOrders/components/Navbar.tsx
+++ src/modules/Projects/routes/Project/outlet/PurchaseOrders/components/Navbar.tsx
@@ -1,16 +1,33 @@
+import Permission from "../../../../../../../common/auth/Permission";
+import { useCurrentUser } from "../../../../../../../hooks";
+import { logisticsTypes } from "../../../../../../../utils";
 import NavbarItemProject from "./NavbarItemProject";
 
 export default function NavbarProject() {
-
+  const { user } = useCurrentUser();
+  
   return (
     <div className="relative flex flex-row gap-4 overflow-auto text-nowrap w-full min-h-12 mb-6 z-10">
       <NavbarItemProject to='' name="Resumen" />
-      <NavbarItemProject to='purchase-orders' name="Órdenes de Compra" />
+      
+      <Permission user={user} allow={logisticsTypes}>  
+        <NavbarItemProject to='purchase-orders' name="Órdenes de Compra" />
+      </Permission>  
+      
       <NavbarItemProject to='requests' name="Requerimientos" />
-      <NavbarItemProject to='petty-cash' name="Caja Chica" />
+      
+      <Permission user={user} allow={logisticsTypes}>
+        <NavbarItemProject to='petty-cash' name="Caja Chica" />
+      </Permission>
+      
       <NavbarItemProject to='emergencies' name="Emergencias" />
-      <NavbarItemProject to='payrolls' name="Planillas / Asistencias" />
+      
+      <Permission user={user} allow={logisticsTypes}>
+        <NavbarItemProject to='payrolls' name="Planillas / Asistencias" />
+      </Permission>
+      
       <NavbarItemProject to='progress' name="Avance" />
+      
       <div className="w-full border-b border-gray-300 absolute bottom-0"></div>
     </div>
   );
